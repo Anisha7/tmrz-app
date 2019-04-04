1dc008043bd054928078691de2a4601ab45e6349
Anisha Jain <anishajain@outlook.com>
2019-04-04 12:34:45 -0700

    persisting timers

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import reducers from './reducers';
 import NewTimer from './components/new-timer';
 import ListTimers from './components/list-timers';
 import { update } from './actions'
+import { loadState, saveState } from './utils'
+import throttle from 'lodash/throttle'
 
-const store = createStore(reducers);
+const persistedState = loadState()
+const store = createStore(reducers, persistedState);
+store.subscribe(throttle(() => {
+  saveState(store.getState())
+}, 1000));
 
 let lastUpdateTime = Date.now()
 setInterval(() => {
